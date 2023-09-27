d3b5e237fcc21d1b734e0f7048eb1739b0765d6c
VICTOR COLORADO GAJARDO <kidacolorado@gmail.com>
2023-09-27 12:28:32 +0200

    agregadas y corregidas cosas en el scanpackages del run

--- src/main/java/com/viewnext/kidaprojects/crudlibros/CrudLibrosApplication.java
+++ src/main/java/com/viewnext/kidaprojects/crudlibros/CrudLibrosApplication.java
@@ -3,7 +3,12 @@ package com.viewnext.kidaprojects.crudlibros;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication (scanBasePackages = {"com.viewnext.kidaprojects.crudlibros.restcontroller", "com.viewnext.kidaprojects.crudlibros.service", "com.viewnext.kidaprojects.crudlibros.mapper" })
+@SpringBootApplication(scanBasePackages = { "com.viewnext.kidaprojects.crudlibros.restcontroller",
+		"com.viewnext.kidaprojects.crudlibros.service",
+		"com.viewnext.kidaprojects.crudlibros.mapper",		
+		"com.viewnext.kidaprojects.crudlibros.exception",
+		"com.viewnext.kidaprojects.crudlibros.dto",
+		"com.viewnext.kidaprojects.crudlibros.model" })
 public class CrudLibrosApplication {
 
 	public static void main(String[] args) {
